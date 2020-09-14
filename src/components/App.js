41bd494a3c20464cf72521e73e8914aefb34cf67
Aleksandar Mladenovic <ml_aleksandar@hotmail.com>
2020-09-14 18:07:48 +0200

    Adding react-bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h4>Hello</h4>
     </div>
   );
 }
