1bca199ff13da158091a5d5bb3bb02eca7f76aa3
Aleksandar Mladenovic <ml_aleksandar@hotmail.com>
2020-09-21 13:37:16 +0200

    Added map icons and some changes to the weapon and map table

--- src/components/WeaponAndMapStats.js
+++ src/components/WeaponAndMapStats.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Table from 'react-bootstrap/Table';
+import Image from 'react-bootstrap/Image';
+import './WeaponAndMapStats.css';
 const WeaponAndMapStats = ({weapons, maps}) => {
     const weaponKeys = Object.keys(weapons);
     const mapKeys = Object.keys(maps);
@@ -11,34 +13,34 @@ const WeaponAndMapStats = ({weapons, maps}) => {
     const calculatePercentage = (numerator, denominator) => ((numerator / denominator) * 100).toFixed(2);
     return (
         <Row>
-           <Col>
+            <Col md="6" sm="12">
                 <Table hover responsive>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Weapon: </th>
-                        <th>Kills: </th>
-                        <th>Shots: </th>
-                        <th>Hits: </th>
-                        <th>Accuracy: </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    { sortedWeaponKeys.slice(0,5).map((key, index) => (
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Weapon: </th>
+                            <th>Kills: </th>
+                            <th>Shots: </th>
+                            <th>Hits: </th>
+                            <th>Accuracy: </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { sortedWeaponKeys.slice(0,5).map((key, index) => (
                         <tr key={index}>
                             <th>{index + 1}</th>
-                            <th><img src={process.env.PUBLIC_URL + 'images/weapon_icons/' + key + '.png'} alt="" width='96px' height='32px' className="img-fluid"/></th>
-                            {/* <th>{key}</th> */}
+                            <th className="d-none d-md-table-cell"><Image src={process.env.PUBLIC_URL + 'images/weapon_icons/' + key + '.png'} alt="" width='96px' height='32px' fluid/></th>
+                            <th className="d-table-cell d-md-none">{key}</th>
                             <th>{weapons[key].total_kills}</th>
                             <th>{weapons[key].total_shots}</th>
                             <th>{weapons[key].total_hits}</th>
                             <th>{ calculatePercentage(weapons[key].total_hits, weapons[key].total_shots) }%</th>
                         </tr>
-                    ))}
-                </tbody>
+                        ))}
+                    </tbody>
                 </Table>
-           </Col>
-           <Col>
+            </Col>
+            <Col md="6" sm="12">
                 <Table hover responsive>
                     <thead>
                         <tr>
@@ -50,20 +52,21 @@ const WeaponAndMapStats = ({weapons, maps}) => {
                         </tr>
                     </thead>
                     <tbody>
-                        {sortedMapKeys.map((key, index) => (
-                            <tr key={index}>
-                                <th>{index + 1}</th>
-                                <th>{key}</th>
-                                <th>{maps[key].total_rounds}</th>
-                                <th>{maps[key].total_wins}</th>
-                                <th>{calculatePercentage(maps[key].total_wins, maps[key].total_rounds, )}</th>
-                            </tr>
-                        ))}
+                        {sortedMapKeys.slice(0,5).map((key, index) => (
+                        <tr key={index}>
+                            <th>{index + 1}</th>
+                            <th className="d-none d-md-table-cell"><Image src={process.env.PUBLIC_URL + 'images/map_icons/collection_icon_' + key + '.png'} width="45px" height="45px" fluid/></th>
+                            <th className="d-table-cell d-md-none">{key}</th>
+                            <th>{maps[key].total_rounds}</th>
+                            <th>{maps[key].total_wins}</th>
+                            <th>{calculatePercentage(maps[key].total_wins, maps[key].total_rounds, )}%</th>
+                        </tr>
+                    ))}
                     </tbody>
                 </Table>
-           </Col>
+            </Col>
         </Row>
-      );
+        );
 }
  
 export default WeaponAndMapStats;
